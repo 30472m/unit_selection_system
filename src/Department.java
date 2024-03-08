b481491d8a2d847fcfff5264308c775f9824df71
username@gmail.com <username@example.com>
2024-03-08 18:10:41 +0330

    create properties and basic methodes for class Department

--- src/Department.java
+++ src/Department.java
@@ -1,2 +1,30 @@
+import java.util.ArrayList;
+
 public class Department {
+
+    private final String deptName;
+    private final ArrayList<Course> courses;
+
+    public Department(String dp_name) {
+        this.deptName = dp_name;
+        this.courses = new ArrayList<>();
+    }
+
+    public void showDeptCourses() {
+        for (Course crs : this.getCourses())
+            System.out.println(crs);
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public ArrayList<Course> getCourses() {
+        return courses;
+    }
+
+    @Override
+    public String toString() {
+        return "   - " + getDeptName();
+    }
 }
