dab83a83bbec3a023cc8c4becd58feeb64da148c
username@gmail.com <username@example.com>
2024-03-08 18:12:11 +0330

    create properties & basic methodes for class Student

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,45 @@
+import java.util.regex.*;
+
 public class Student {
+    private final String studentNum;
+    private int numOfGnUnits;
+    private int numOfUnits;
+
+    public Student(String st_num) {
+        this.studentNum = st_num;
+        this.numOfGnUnits = 0;
+        this.numOfUnits = 0;
+    }
+
+    public static boolean checkStudNumFormat(String inp) {
+        String regex = "^[1-9][0-9]{8}$";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(inp);
+        return m.matches();
+    }
+
+    public String getStudentNum() {
+        return studentNum;
+    }
+
+    public int getNumOfUnits() {
+        return numOfUnits;
+    }
+
+    public void setNumOfUnits(int units) {
+        this.numOfUnits = units;
+    }
+
+    public int getNumOfGnUnits() {
+        return numOfGnUnits;
+    }
+
+    public void setNumOfGnUnits(int gnUnits) {
+        this.numOfGnUnits = gnUnits;
+    }
+
+    @Override
+    public String toString() {
+        return "   ----: " + getStudentNum();
+    }
 }
