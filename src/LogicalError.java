831dce08bc5135dcef806bd5ad58c0c5d94fedd7
username@gmail.com <username@example.com>
2024-03-08 20:06:07 +0330

    create LogicalError class and create basic methods in

---
+++ src/LogicalError.java
@@ -0,0 +1,32 @@
+public class LogicalError {
+
+    public static void errorLoginPage() {
+    }
+
+    public static void errorUserSelectDepts() {
+    }
+
+    public static void errorAdminManageCourses() {
+    }
+
+    public static void errorAdminManageStudents() {
+    }
+
+    public static void errorStudentManageCourses() {
+    }
+
+    public static void errorStudentCoursesList() {
+    }
+
+    public static void errorStudentDeptCourses() {
+    }
+
+    public static void errorExistStudCrsEnroll() {
+    }
+
+    public static void errorSumOfUnitsGt20() {
+    }
+
+    public static void errorSumOfGnUnitsGt5() {
+    }
+}
